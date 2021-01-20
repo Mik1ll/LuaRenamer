03a3ccf900c0d441419d698c6d3291c3fb1357cc
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2021-01-19 19:21:17 -0800

    mostly working (if you add dll)

--- ScriptRenamer/ScriptRenamer.cs
+++ ScriptRenamer/ScriptRenamer.cs
@@ -1,6 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Reflection;
 using Antlr4.Runtime;
 using Shoko.Plugin.Abstractions;
 using Shoko.Plugin.Abstractions.Attributes;
@@ -11,11 +12,71 @@ namespace ScriptRenamer
     [Renamer("ScriptRenamer")]
     class ScriptRenamer : IRenamer
     {
+        static ScriptRenamer()
+        {
+            EmbedDll();
+        }
         public (IImportFolder destination, string subfolder) GetDestination(MoveEventArgs args)
         {
-            AntlrInputStream inputStream = new(new StreamReader(args.Script.Script, Encoding.UTF8));
-            var context = SetupContext(inputStream);
-            var visitor = new ScriptRenamerVisitor
+            if (string.IsNullOrWhiteSpace(args.Script?.Script))
+            {
+                return (null, null);
+            }
+            var (context, visitor) = GetContext(new MoveEventArgs
+            {
+                AnimeInfo = args.AnimeInfo,
+                EpisodeInfo = args.EpisodeInfo,
+                FileInfo = args.FileInfo,
+                GroupInfo = args.GroupInfo,
+                Script = args.Script
+            });
+            _ = visitor.Visit(context);
+            return (args.AvailableFolders.FirstOrDefault(f => f.Name == visitor.Destination), visitor.Subfolder);
+        }
+
+
+        public string GetFilename(RenameEventArgs args)
+        {
+            if (string.IsNullOrWhiteSpace(args.Script?.Script))
+            {
+                return null;
+            }
+            var (context, visitor) = GetContext(new MoveEventArgs
+            {
+                AnimeInfo = args.AnimeInfo,
+                EpisodeInfo = args.EpisodeInfo,
+                FileInfo = args.FileInfo,
+                GroupInfo = args.GroupInfo,
+                Script = args.Script
+            });
+            _ = visitor.Visit(context);
+            return visitor.Filename + Path.GetExtension(args.FileInfo.Filename);
+        }
+
+        private static void EmbedDll()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+            {
+                if (new AssemblyName(args.Name).Name != "Antlr4.Runtime.Standard")
+                    return null;
+                string resourceName = Assembly.GetExecutingAssembly().GetName().Name + ".Antlr4.Runtime.Standard.dll";
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+                {
+                    byte[] assemblyData = new byte[stream.Length];
+                    stream.Read(assemblyData, 0, assemblyData.Length);
+                    return Assembly.Load(assemblyData);
+                }
+            };
+        }
+
+
+        private static (ParserRuleContext context, ScriptRenamerVisitor visitor) GetContext(MoveEventArgs args)
+        {
+            AntlrInputStream inputStream = new(new StringReader(args.Script.Script));
+            ScriptRenamerLexer lexer = new(inputStream);
+            CommonTokenStream tokenStream = new(lexer);
+            ScriptRenamerParser parser = new(tokenStream);
+            return (parser.start(), new ScriptRenamerVisitor
             {
                 AvailableFolders = args.AvailableFolders,
                 AnimeInfo = args.AnimeInfo.FirstOrDefault(),
@@ -23,33 +84,7 @@ namespace ScriptRenamer
                 FileInfo = args.FileInfo,
                 GroupInfo = args.GroupInfo.FirstOrDefault(),
                 Script = args.Script
-            };
-            _ = visitor.Visit(context);
-            return (args.AvailableFolders.FirstOrDefault(f => f.Name == visitor.Destination), visitor.Subfolder);
-        }
-
-        public string GetFilename(RenameEventArgs args)
-        {
-            AntlrInputStream inputStream = new(new StreamReader(args.Script.Script, Encoding.UTF8));
-            var context = SetupContext(inputStream);
-            var visitor = new ScriptRenamerVisitor
-            {
-                AnimeInfo = args.AnimeInfo.FirstOrDefault(),
-                EpisodeInfo = args.EpisodeInfo.FirstOrDefault(),
-                FileInfo = args.FileInfo,
-                GroupInfo = args.GroupInfo.FirstOrDefault(),
-                Script = args.Script
-            };
-            _ = visitor.Visit(context);
-            return visitor.Filename;
-        }
-
-        private static ParserRuleContext SetupContext(AntlrInputStream inputStream)
-        {
-            ScriptRenamerLexer lexer = new(inputStream);
-            CommonTokenStream tokenStream = new(lexer);
-            ScriptRenamerParser parser = new(tokenStream);
-            return parser.start();
+            });
         }
     }
 }
