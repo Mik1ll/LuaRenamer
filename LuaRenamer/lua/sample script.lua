8693b9f701e0416e85fa13ee757242d501bf69cc
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2022-07-01 12:18:37 -0700

    update sample script.lua

--- LuaRenamer/lua/sample script.lua
+++ LuaRenamer/lua/sample script.lua
@@ -1,21 +1,29 @@
-function rm_empty_str(table) return from(table):where(function (a) return a ~= "" end):toArray() end
+replace_illegal_chars = true
+
+local maxnamelen = 35
+local animelanguage = Language.English
+local episodelanguage = Language.English
+
 
 local group = file.anidb and file.anidb.releasegroup and "[" .. (file.anidb.releasegroup.shortname or file.anidb.releasegroup.name) .. "]" or ""
 
-local animename = anime:getname(Language.English) or anime:getname(Language.Romaji) or anime.preferredname
-animename = string.gsub(string.sub(animename, 0, 35), "%s+$", "") .. (#animename > 35 and "..." or "")
+local animename = anime:getname(animelanguage) or anime.preferredname
+if utf8.len(animename) > maxnamelen then
+  animename = animename:sub(1, utf8.offset(animename, maxnamelen+1, 1)-1):gsub("%s+$", "") .. "..."
+end
 
-local episodename = episode:getname(Language.English) or ""
-episodename = string.gsub(string.sub(episodename, 0, 35), "%s+$", "") .. (#episodename > 35 and "..." or "")
-
+local episodename = ""
+local engepname = episode:getname(Language.English) or ""
 local episodenumber = ""
-if anime.type ~= AnimeType.Movie or not string.find(episodename, "Complete Movie") then
-  episodenumber = episode_numbers(2) .. (file.anidb and file.anidb.version > 1 and "v" .. file.anidb.version or "")
-  if #episodes > 1 or string.find(episodename, "Episode") then
-    episodename = ""
+if anime.type ~= AnimeType.Movie or not engepname:find("^Complete Movie") then
+  local maxepnum = #tostring(fromDictionary(anime.episodecounts):select(function(kvp) return kvp.value; end):max())
+  episodenumber = episode_numbers(maxepnum) .. (file.anidb and file.anidb.version > 1 and "v" .. file.anidb.version or "")
+  if #episodes == 1 and not engepname:find("^Episode") then
+    episodename = episode:getname(episodelanguage) or ""
+    if utf8.len(episodename) > maxnamelen then
+        episodename = episodename:sub(1, utf8.offset(episodename, maxnamelen+1, 1)-1):gsub("%s+$", "") .. "..."
+    end
   end
-else
-  episodename = ""
 end
 
 local res = file.media.video.res or ""
@@ -23,6 +31,7 @@ local codec = file.media.video.codec or ""
 local bitdepth = file.media.video.bitdepth and file.media.video.bitdepth ~= 8 and file.media.video.bitdepth .. "bit" or ""
 local source = file.anidb and file.anidb.source or ""
 
+function rm_empty_str(table) return from(table):where(function (a) return a ~= "" end):toArray() end
 local fileinfo = "(" .. table.concat(rm_empty_str{res, codec, bitdepth, source}, " ") .. ")"
 
 local dublangs = from(file.anidb and file.anidb.media.dublanguages or from(file.media.audio):select("language"))
@@ -34,4 +43,4 @@ local centag = anime.restricted and file.anidb and (file.anidb.censored and "[CE
 local hashtag = "[" .. file.hashes.crc .. "]"
 
 local nametable = rm_empty_str{group, animename, episodenumber, episodename, fileinfo, audiotag, subtag, centag, hashtag}
-filename = string.sub(table.concat(nametable, " "), 0, 120)
+filename = table.concat(nametable, " ")
